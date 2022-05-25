3ed2b50c514595973c8862b0d07a0c2478307ba4
Dayan Arango <dayaara6@gmail.com>
2022-05-25 16:23:49 -0400

    añadiendo bedidas y limites

--- components/ModalProducto.js
+++ components/ModalProducto.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import useQuisco from "../hooks/useQuisco";
 import { formatear } from "../helpers";
+import { useState } from "react";
 const ModalProducto = () => {
   const { producto, handleChangeModal } = useQuisco();
+  const [cantidad, setCantidad] = useState(1)
   return (
     <div className="md:flex gap-10">
       <div className="md:w-1/3">
@@ -16,7 +18,9 @@ const ModalProducto = () => {
       <div className="md:w-2/3">
         <div className="flex justify-end">
           <button
-          onClick={()=>{handleChangeModal()}}
+            onClick={() => {
+              handleChangeModal();
+            }}
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -38,6 +42,53 @@ const ModalProducto = () => {
         <p className="mt-5 font-black text-5xl text-amber-500">
           {formatear(producto.precio)}
         </p>
+        <div className="flex gap-4 mt-5">
+          <button
+          type="button"
+          onClick={()=>{
+             if(cantidad <= 1) return
+            setCantidad(cantidad-1)
+          }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-7 w-7"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
+          <p className="text-3xl">{cantidad}</p>
+          <button
+          type="button"
+          onClick={()=>{
+            if(cantidad >= 5) return
+            setCantidad(cantidad+1)
+          }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-7 w-7"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   );
